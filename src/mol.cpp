484a434551ad9516a63d286d1210a3eb4409d443
Riccardo Vianello <riccardo.vianello@gmail.com>
2021-01-29 23:00:08 +0000

    add mol binary i/o

--- src/mol.cpp
+++ src/mol.cpp
@@ -55,29 +55,27 @@ mol_out(PG_FUNCTION_ARGS)
   PG_RETURN_CSTRING(pnstrdup(smiles.c_str(), smiles.size()));
 }
 
-/*
-
 Datum
 mol_recv(PG_FUNCTION_ARGS)
 {
-    StringInfo  buf = (StringInfo) PG_GETARG_POINTER(0);
-    Complex    *result;
+  bytea *data = PG_GETARG_BYTEA_PP(0);
 
-    result = (Complex *) palloc(sizeof(Complex));
-    result->x = pq_getmsgfloat8(buf);
-    result->y = pq_getmsgfloat8(buf);
-    PG_RETURN_POINTER(result);
+  std::string pkl;
+  pkl.assign(VARDATA_ANY(data), VARSIZE_ANY_EXHDR(data));
+  auto *mol = new RDKit::ROMol(pkl);
+
+  if (!mol) {
+    ereport(ERROR,
+            errcode(ERRCODE_DATA_EXCEPTION),
+            errmsg("could not construct molecule"));
+  }
+
+  PG_RETURN_BYTEA_P(data);
 }
 
 Datum
 mol_send(PG_FUNCTION_ARGS)
 {
-    Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
-    StringInfoData buf;
-
-    pq_begintypsend(&buf);
-    pq_sendfloat8(&buf, complex->x);
-    pq_sendfloat8(&buf, complex->y);
-    PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
+  bytea *data = PG_GETARG_BYTEA_PP(0);
+  PG_RETURN_BYTEA_P(data);
 }
-*/
--- src/ribollita--0.0.sql
+++ src/ribollita--0.0.sql
@@ -19,24 +19,39 @@ CREATE FUNCTION mol_out(mol)
     AS 'MODULE_PATHNAME', 'mol_out'
     LANGUAGE C IMMUTABLE STRICT;
 
+CREATE FUNCTION mol_recv(internal)
+    RETURNS mol
+    AS 'MODULE_PATHNAME', 'mol_recv'
+    LANGUAGE C STRICT IMMUTABLE;
+
+CREATE FUNCTION mol_send(mol)
+    RETURNS bytea
+    AS 'MODULE_PATHNAME', 'mol_send'
+    LANGUAGE C STRICT IMMUTABLE;
+
 CREATE TYPE mol (
    internallength = variable,
    input = mol_in,
    output = mol_out,
+   receive = mol_recv,
+   send = mol_send,
    storage = extended
 );
 
 CREATE FUNCTION mol_amw(mol)
     RETURNS float8
+    PARALLEL SAFE
     AS 'MODULE_PATHNAME', 'mol_amw'
     LANGUAGE C IMMUTABLE STRICT;
 
 CREATE FUNCTION mol_from_smiles(cstring)
     RETURNS mol
+    PARALLEL SAFE
     AS 'MODULE_PATHNAME', 'mol_from_smiles'
     LANGUAGE C IMMUTABLE STRICT;
 
 CREATE FUNCTION mol_to_smiles(mol)
     RETURNS cstring
+    PARALLEL SAFE
     AS 'MODULE_PATHNAME', 'mol_to_smiles'
     LANGUAGE C IMMUTABLE STRICT;
