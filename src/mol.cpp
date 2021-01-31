e9b0bbbd1d29ca8296e482cd52563c19f7fc8719
Riccardo Vianello <riccardo.vianello@gmail.com>
2021-01-31 13:16:27 +0000

    migrate mol text i/o from smiles to base64

--- src/mol.cpp
+++ src/mol.cpp
@@ -1,12 +1,10 @@
-#include <GraphMol/RDKitBase.h>
 #include <GraphMol/MolPickler.h>
-#include <GraphMol/SmilesParse/SmilesParse.h>
-#include <GraphMol/SmilesParse/SmilesWrite.h>
 
 extern "C" {
 
 #include <postgres.h>
 #include <fmgr.h>
+#include <common/base64.h>
 #include <libpq/pqformat.h>
 
 PG_FUNCTION_INFO_V1(mol_in);
@@ -20,24 +18,33 @@ PG_FUNCTION_INFO_V1(mol_send);
 Datum
 mol_in(PG_FUNCTION_ARGS)
 {
-  char *data = PG_GETARG_CSTRING(0);
-  bytea *result;
+  char *text = PG_GETARG_CSTRING(0);
+  int text_len = strlen(text);
+
+  int data_len = pg_b64_dec_len(text_len);
+  char *data = (char *) palloc(data_len);
+  data_len = pg_b64_decode(text, text_len, data, data_len);
+
+  if (data_len < 0) {
+    ereport(ERROR,
+            errcode(ERRCODE_DATA_EXCEPTION),
+            errmsg("could not decode molecule data as base64"));
+  }
 
   try {
-    auto *mol = RDKit::SmilesToMol(data);
-    std::string pkl;
-    RDKit::MolPickler::pickleMol(mol, pkl);
+    std::string pkl(data, data_len);
+    auto *mol = new RDKit::ROMol(pkl);
     delete mol;
-    auto sz = pkl.size();
-    result = (bytea *) palloc(VARHDRSZ + sz);
-    memcpy(VARDATA(result), pkl.data(), sz);
-    SET_VARSIZE(result, VARHDRSZ + sz);
   }
   catch (...) {
     ereport(ERROR,
             errcode(ERRCODE_DATA_EXCEPTION),
             errmsg("could not construct molecule"));
   }
+
+  bytea *result = (bytea *) palloc(VARHDRSZ + data_len);
+  memcpy(VARDATA(result), data, data_len);
+  SET_VARSIZE(result, VARHDRSZ + data_len);
 
   PG_RETURN_BYTEA_P(result);
 }
@@ -47,12 +54,18 @@ mol_out(PG_FUNCTION_ARGS)
 {
   bytea *data = PG_GETARG_BYTEA_PP(0);
 
-  std::string pkl(VARDATA_ANY(data), VARSIZE_ANY_EXHDR(data));
-  auto *mol = new RDKit::ROMol(pkl);
-  std::string smiles = RDKit::MolToSmiles(*mol, true);
-  delete mol;
+  int dstlen = pg_b64_enc_len(VARSIZE_ANY_EXHDR(data));
+  char *result = (char *) palloc(dstlen + 1);
 
-  PG_RETURN_CSTRING(pnstrdup(smiles.c_str(), smiles.size()));
+  dstlen = pg_b64_encode(VARDATA_ANY(data), VARSIZE_ANY_EXHDR(data), result, dstlen);
+  if (dstlen < 0) {
+    ereport(ERROR,
+            errcode(ERRCODE_DATA_EXCEPTION),
+            errmsg("could not encode molecule data as base64"));
+  }
+  result[dstlen] = '\0';
+
+  PG_RETURN_CSTRING(result);
 }
 
 Datum
@@ -81,6 +94,6 @@ mol_recv(PG_FUNCTION_ARGS)
 Datum
 mol_send(PG_FUNCTION_ARGS)
 {
-  bytea *data = PG_GETARG_BYTEA_PP(0);
+  bytea *data = PG_GETARG_BYTEA_P_COPY(0);
   PG_RETURN_BYTEA_P(data);
 }
